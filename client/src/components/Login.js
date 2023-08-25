d67c797aae5905af8cd7dc3c700090505b2d2423
ankur1493 <ankursharma1493@gmail.com>
2023-08-25 16:49:19 +0530

    ui for signup and login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import "./login.css"
 const Login = () => {
   return (
-    <div>Login</div>
-  )
+    <div className='login'>
+      <div className="head">Log In</div>
+      <form>
+        <input type='text' name="organization" placeholder='Enter your organization Name' />
+        <input type='text' name="email" placeholder='Enter your email' />
+        <input type='password' name="password" placeholder='Enter your Password' />
+        <button>Login</button>
+      </form>
+      
+    </div>  )
 }
 
 export default Login
