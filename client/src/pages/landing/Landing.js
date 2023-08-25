a32aea4287d86649541af96e37abf9eec2dea620
ankur1493 <ankursharma1493@gmail.com>
2023-08-25 15:52:31 +0530

    added Landing page functionality

--- client/src/pages/landing/Landing.js
+++ client/src/pages/landing/Landing.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./landing.css"
+import SignUp from "../../components/SignUp";
+import Login from "../../components/Login";
 
 const Landing = () => {
+
+    const [alreadySigned , setAlreadySigned] = useState(false); 
+
+    const handleSign = ()=>{
+        setAlreadySigned(!alreadySigned);
+    }
+    
   return (
-    <div>Landing</div>
+    <div className='landing'>
+        <div className="heroSection">
+            <div className="leftArea">
+                <div className="logo">
+                    Space <span>Hack</span>
+                </div>
+                <div className="image">
+                    <img src="./spaceImage.jpg" alt="" />
+                </div>
+            </div>
+            <div className="rightArea">
+                {alreadySigned ? <Login/> : <SignUp/>}
+                or
+                <button onClick={()=>{handleSign()}}>{alreadySigned ? "First Time Here, Sign Up" : "already a user, Login"}</button>
+            </div>
+        </div>
+        
+    </div>
   )
 }
 
