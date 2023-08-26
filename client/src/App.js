ed481833df1261e264998591af2fe9385c3eebf9
ankur1493 <ankursharma1493@gmail.com>
2023-08-26 08:38:24 +0530

    created home page

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Landing from './pages/landing/Landing';
 import Home from './pages/home/Home';
-import Proposal from './pages/proposal/Proposal';
-import Satellite from './pages/satellite/Satellite';
 
 function App() {
   const user = window.localStorage.getItem("userID");
@@ -14,8 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={!user ? <Landing /> : <Navigate to="/home" />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/proposal" element={<Proposal />} />
-          <Route path="/satellites" element={<Satellite />} />
         </Routes>
       </Router>
     </div>
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import "./home.css"
 import Navbar from '../../components/Navbar'
+import Proposal from "../proposal/Proposal"
 
 const Home = () => {
   return (
-    <div>
-        <Navbar/>
+    <div className='home'>
+      <div className="header">
+        <Navbar background = "transparent"/>
+        <div className="info">
+          <div className="heading">Space</div>
+          <div className="heading">Sync</div>
+          <div className="infoDetails">Work with Government Organizations on Satellite design, Propulsion tech, or space missions</div>
+        </div>
+      </div>
+      <div className="options">
+          <img src="./subImage1.png" alt="" />
+          <img src="./subImage2.png" alt="" />
+          <img src="./subImage3.png" alt="" />
+      </div>
+      <div className="contributePage">
+        <Proposal/>
+      </div>
+
     </div>
   )
 }
